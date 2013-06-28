ae853bed062e6c8cc31508d623fd28c6110c5fce
Russell Matney <russell.matney@gmail.com>
2013-06-28 17:54:19 -0400

    adds question

--- ruby/1.rb
+++ ruby/1.rb
@@ -1,4 +1,9 @@
-def multsOf3and5(max)
+# If we list all the natural numbers below 10 that are multiples 
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+def sumOfMultsOf3and5(max)
   sum = 0
   for i in 0..max-1
     if i % 3 == 0 || i % 5 == 0
@@ -8,4 +13,4 @@ def multsOf3and5(max)
   sum
 end
 
-puts multsOf3and5(1000)
+puts sumOfMultsOf3and5(1000)
