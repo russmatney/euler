b353269c9c7613634a89b525844f0acd87a081eb
Russell Matney <russell.matney@gmail.com>
2013-07-10 23:25:15 -0400

    disgusting brute force of number 5

---
+++ ruby/5.rb
@@ -0,0 +1,30 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+
+def IsDivisibleUpTo(x)
+  # checks if it is divisible up to max
+  max = 20
+  for i in 2..max
+    if x % i != 0
+      return false
+    end
+  end
+  return true
+end
+
+def brutishForcish(tldr)
+  ans = []
+  for i in 2..tldr
+    if IsDivisibleUpTo(i)
+      ans << i
+    end
+    if ans.length > 0
+      return i
+    end
+  end
+end
+
+puts brutishForcish(252000000)
